f49d5ab7370409ed7bbab9b5f11873322d6b6099
Daria Krepchuk <119439696+evikary@users.noreply.github.com>
2024-03-13 21:01:03 +0300

    init: RSS-PZ-00 init project (#27)
    * init: add typescript and webpack
    
    * feat: RSS-PZ-00 add eslint, prettier, husky

---
+++ rss-puzzle/.eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['eslint:recommended', 'airbnb-base', 'airbnb-typescript/base', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['import', '@typescript-eslint'],
+  rules: {
+    "@typescript-eslint/no-explicit-any": "error"
+  },
+};
---
+++ rss-puzzle/webpack.config.js
@@ -0,0 +1,43 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.ts",
+  devtool: process.env.MODE === "production" ? false : "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(png|jpe?g|svg|gif|mp3)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "assets/[name][ext][query]",
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ title: "rss-puzzle" }),
+    new MiniCssExtractPlugin({
+      filename: "css/[name].css",
+    }),
+  ],
+  devServer: { port: 4001 },
+};
